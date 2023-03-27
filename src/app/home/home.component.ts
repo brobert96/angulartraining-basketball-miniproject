bc972faba0cb0ae5ce231a41f4d59395456f5e28
Robert Balazs <balazsrobert96@gmail.com>
2023-03-27 11:50:46 +0300

    updated subscription to be unsubscribed on destroy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Team } from '../shared/models';
 import { GamesService } from '../shared/services';
 import { TeamsService } from '../shared/services/teams/teams.service';
@@ -6,31 +7,43 @@ declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   teamList: Team[] = [];
   trackedTeams: Team[] = [];
-  constructor(private teamService: TeamsService, private gameService: GamesService) { }
+  subscriptions: Subscription = new Subscription();
+  constructor(
+    private teamService: TeamsService,
+    private gameService: GamesService
+  ) {}
 
   ngOnInit(): void {
     this.teamService.getTeams().subscribe((data: Team[]) => {
       this.teamList = data;
     });
 
-    this.teamService.trackedTeams$.subscribe((data: Team[]) => {
-      this.trackedTeams = data;
-    });
+    this.subscriptions.add(
+      this.teamService.trackedTeams$.subscribe((data: Team[]) => {
+        this.trackedTeams = data;
+      })
+    );
+  }
+    
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   trackTeam(id: string) {
     let teamId = parseInt(id);
     const team = this.teamList.find((t: Team) => t.id === teamId);
-    if(team){
-      this.gameService.getSimpleGamesFromPast12Days(teamId).subscribe((data: any) => {
-        team.simpleGameResults = data;
-        this.teamService.trackTeam(team);
-      });
+    if (team) {
+      this.gameService
+        .getSimpleGamesFromPast12Days(teamId)
+        .subscribe((data: any) => {
+          team.simpleGameResults = data;
+          this.teamService.trackTeam(team);
+        });
     }
   }
 }
